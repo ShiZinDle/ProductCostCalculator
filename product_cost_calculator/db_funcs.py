3a96230b08b15d248acc28fe6ea4cd3111a3d83c
Yuval Cagan <yuval.cagan@gmail.com>
2020-12-10 22:26:52 +0200

    Profile fixed, removed supplies and cost

--- product_cost_calculator/db_funcs.py
+++ product_cost_calculator/db_funcs.py
@@ -10,6 +10,162 @@ from product_cost_calculator import login_manager, session
 from product_cost_calculator.mappings import Ingredient, Product, Recipe, Unit, User
 
 
+# Ingredient
+def add_ingredient(name: str, unit_id: int) -> int:
+    s = session()
+    ingredient_id = get_ingredient_id(name, unit_id)
+    if ingredient_id:
+        return ingredient_id
+    ingredient = Ingredient(name=name.lower(), unit_id=unit_id)
+    s.add(ingredient)
+    s.commit()
+    return ingredient.ingredient_id
+
+
+def get_ingredient_id(name: str, unit_id: int) -> Optional[int]:
+    s = session()
+    ingredient = s.query(Ingredient).filter(and_(Ingredient.name==name.lower(),
+                                           Ingredient.unit_id==unit_id)).first()
+    try:
+        return ingredient.ingredient_id
+    except AttributeError:
+        return None
+
+
+def get_ingredient_name(ingredient_id: int) -> str:
+    s = session()
+    return s.query(Ingredient).get(ingredient_id).name
+
+
+def get_ingredient_unit(ingredient_id: int) -> str:
+    s = session()
+    ingredient = s.query(Ingredient).get(ingredient_id)
+    return get_unit_name(ingredient.unit_id)
+
+
+
+# Product
+def get_product_id(name: str) -> int:
+    s = session()
+    product = s.query(Product).filter(and_(Product.product_id==current_user.user_id,
+                                           Product.name==name.lower())).first()
+    return product.product_id
+
+
+def get_product(product_id: int) -> Dict[str, Union[bool, int, str]]:
+    s = session()
+    product = s.query(Product).get(product_id)
+    return {'product_id': product.product_id,
+            'name': product.name,
+            'amount': product.amount,
+            'unit': get_unit_name(product.unit_id),
+            'cost': get_product_cost(product_id),
+            'user_id': product.user_id,
+            'username': get_user(product.user_id).username,
+            'public': product.public == 1}
+
+
+def add_product(name: str, amount: int, unit: int, public: bool) -> int:
+    s = session()
+    product = Product(name=name.lower(), amount=amount, unit_id=unit,
+                      user_id=current_user.user_id, public=public)
+    s.add(product)
+    s.commit()
+    return product.product_id
+
+
+def delete_product(product_id: int) -> None:
+    s = session()
+    product = s.query(Product).filter(Product.product_id==product_id).first()
+    s.delete(product)
+    s.commit()
+
+
+def get_all_products(user_id: int, public_only: bool = False) -> List[Dict[str, Union[int, str]]]:
+    s = session()
+    if public_only:
+        products = s.query(Product.product_id).filter(and_(Product.user_id==user_id,
+                                                        Product.public==public_only)).all()
+    else:
+        products = s.query(Product.product_id).filter(Product.user_id==user_id).all()
+    all_products = [get_product(product) for product in products]
+    return sorted(all_products, key=lambda product: product['name'])
+
+
+def get_all_public_products() -> List[Dict[str, Union[int, str]]]:
+    s = session()
+    products = s.query(Product.product_id).filter(Product.public==1).all()
+    all_products = [get_product(product) for product in products]
+    shuffle(all_products)
+    return all_products
+
+
+def get_product_cost(product_id: int) -> int:
+    pass
+
+
+def share_product(product_id: int) -> None:
+    s = session()
+    product = s.query(Product).get(product_id)
+    product.public = not product.public
+    s.commit()
+
+
+# Recipe
+def add_recipe(product_id: int, ingredient: str, amount: int, unit_id: int) -> None:
+    s = session()
+    ingredient_id = add_ingredient(ingredient, unit_id)
+    recipe = Recipe(product_id=product_id, ingredient_id=ingredient_id, amount=amount)
+    s.add(recipe)
+    s.commit()
+
+
+def get_recipe(product_id: int) -> List[Dict[str, Union[int, str]]]:
+    s = session()
+    entries = s.query(Recipe).filter(Recipe.product_id==product_id).all()
+    return [{'ingredient_id': entry.ingredient_id,
+             'ingredient': get_ingredient_name(entry.ingredient_id),
+             'amount': entry.amount,
+             'unit': get_ingredient_unit(entry.ingredient_id),
+             } for entry in entries]
+
+
+def delete_recipe(product_id: int, ingredient_id: int) -> None:
+    s = session()
+    recipe = s.query(Recipe).filter(and_(Recipe.product_id==product_id,
+                                         Recipe.ingredient_id==ingredient_id)).first()
+    s.delete(recipe)
+    s.commit()
+
+
+# Unit
+def get_unit_name(unit_id: int) -> str:
+    s = session()
+    unit = s.query(Unit).get(unit_id)
+    if unit.symbol:
+        return unit.symbol
+    return f' {unit.name}'
+
+
+def get_unit_id(name_or_symbol: str) -> int:
+    s = session()
+    unit = s.query(Unit).filter(or_(Unit.name==name_or_symbol.lower(),
+                                    Unit.symbol==name_or_symbol.lower())).first()
+    return unit.unit_id
+
+
+def get_all_units() -> List[str]:
+    s = session()
+    units = s.query(Unit).all()
+    all_units = []
+    for unit in units:
+        if unit.symbol:
+            all_units.append(unit.symbol)
+        else:
+            all_units.append(unit.name)
+    return all_units
+
+
 # User
 def add_user(username: str, email: str, password: str,
              fullname: str, birthday: Optional[date]) -> int:
@@ -79,147 +235,3 @@ def change_birthday(user_id: int, birthday: str) -> None:
     user = s.query(User).filter(User.user_id==user_id).first()
     user.date_of_birth = birthday
     s.commit()
-
-
-# Product
-def get_product_id(name: str) -> int:
-    s = session()
-    product = s.query(Product).filter(and_(Product.product_id==current_user.user_id,
-                                           Product.name==name.lower())).first()
-    return product.product_id
-
-
-def get_product(product_id: int) -> Dict[str, Union[bool, int, str]]:
-    s = session()
-    product = s.query(Product).get(product_id)
-    return {'product_id': product.product_id,
-            'name': product.name,
-            'amount': product.amount,
-            'unit': get_unit_name(product.unit_id),
-            'cost': get_product_cost(product_id),
-            'user_id': product.user_id,
-            'username': get_user(product.user_id).username,
-            'public': product.public == 1}
-
-
-def add_product(name: str, amount: int, unit: int, public: bool) -> int:
-    s = session()
-    product = Product(name=name.lower(), amount=amount, unit_id=unit,
-                      user_id=current_user.user_id, public=public)
-    s.add(product)
-    s.commit()
-    return product.product_id
-
-
-def delete_product(product_id: int) -> None:
-    s = session()
-    product = s.query(Product).filter(Product.product_id==product_id).first()
-    s.delete(product)
-    s.commit()
-
-
-def get_all_products() -> List[Dict[str, Union[int, str]]]:
-    s = session()
-    products = s.query(Product.product_id).filter(Product.user_id==current_user.user_id).all()
-    all_products = [get_product(product) for product in products]
-    return sorted(all_products, key=lambda product: product['name'])
-
-
-def get_all_public_products() -> List[Dict[str, Union[int, str]]]:
-    s = session()
-    products = s.query(Product.product_id).filter(Product.public==1).all()
-    all_products = [get_product(product) for product in products]
-    shuffle(all_products)
-    return all_products
-
-
-def get_product_cost(product_id: int) -> int:
-    pass
-
-
-# Recipe
-def add_recipe(product_id: int, ingredient: str, amount: int, unit_id: int) -> None:
-    s = session()
-    ingredient_id = add_ingredient(ingredient, unit_id)
-    recipe = Recipe(product_id=product_id, ingredient_id=ingredient_id, amount=amount)
-    s.add(recipe)
-    s.commit()
-
-
-def get_recipe(product_id: int) -> List[Dict[str, Union[int, str]]]:
-    s = session()
-    entries = s.query(Recipe).filter(Recipe.product_id==product_id).all()
-    return [{'ingredient_id': entry.ingredient_id,
-             'ingredient': get_ingredient_name(entry.ingredient_id),
-             'amount': entry.amount,
-             'unit': get_ingredient_unit(entry.ingredient_id),
-             } for entry in entries]
-
-
-def delete_recipe(product_id: int, ingredient_id: int) -> None:
-    s = session()
-    recipe = s.query(Recipe).filter(and_(Recipe.product_id==product_id,
-                                         Recipe.ingredient_id==ingredient_id)).first()
-    s.delete(recipe)
-    s.commit()
-
-
-# Ingredient
-def add_ingredient(name: str, unit_id: int) -> int:
-    s = session()
-    ingredient_id = get_ingredient_id(name, unit_id)
-    if ingredient_id:
-        return ingredient_id
-    ingredient = Ingredient(name=name.lower(), unit_id=unit_id)
-    s.add(ingredient)
-    s.commit()
-    return ingredient.ingredient_id
-
-
-def get_ingredient_id(name: str, unit_id: int) -> Optional[int]:
-    s = session()
-    ingredient = s.query(Ingredient).filter(and_(Ingredient.name==name.lower(),
-                                           Ingredient.unit_id==unit_id)).first()
-    try:
-        return ingredient.ingredient_id
-    except AttributeError:
-        return None
-
-
-def get_ingredient_name(ingredient_id: int) -> str:
-    s = session()
-    return s.query(Ingredient).get(ingredient_id).name
-
-
-def get_ingredient_unit(ingredient_id: int) -> str:
-    s = session()
-    ingredient = s.query(Ingredient).get(ingredient_id)
-    return get_unit_name(ingredient.unit_id)
-
-
-# Unit
-def get_unit_name(unit_id: int) -> str:
-    s = session()
-    unit = s.query(Unit).get(unit_id)
-    if unit.symbol:
-        return unit.symbol
-    return f' {unit.name}'
-
-
-def get_unit_id(name_or_symbol: str) -> int:
-    s = session()
-    unit = s.query(Unit).filter(or_(Unit.name==name_or_symbol.lower(),
-                                    Unit.symbol==name_or_symbol.lower())).first()
-    return unit.unit_id
-
-
-def get_all_units() -> List[str]:
-    s = session()
-    units = s.query(Unit).all()
-    all_units = []
-    for unit in units:
-        if unit.symbol:
-            all_units.append(unit.symbol)
-        else:
-            all_units.append(unit.name)
-    return all_units
--- product_cost_calculator/views.py
+++ product_cost_calculator/views.py
@@ -1,6 +1,8 @@
+from typing import Union
 from flask import flash, redirect, render_template, url_for
 from flask_login import current_user, login_user, logout_user
 from flask_login.utils import login_required
+from werkzeug.wrappers import Response
 
 import product_cost_calculator.db_funcs as db_funcs
 from product_cost_calculator import app
@@ -8,46 +10,15 @@ from product_cost_calculator.forms import BirthdayForm, EmailForm, LoginForm, Na
 
 
 @app.route('/')
-def home():
+def home() -> Response:
     products = db_funcs.get_all_public_products()
     return render_template('home.j2', products=products)
 
 
-@app.route('/register/', methods=['GET', 'POST'])
-def register() -> str:
-    form = RegisterForm()
-    if form.validate_on_submit():
-        db_funcs.add_user(username=form.username.data, password=form.password.data,
-                          email=form.email.data, fullname=form.fullname.data,
-                          birthday=form.birthday.data)
-        flash(f'Account created for {form.username.data}.', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.j2', form=form)
-
-
-@app.route('/login/', methods=['GET', 'POST'])
-def login() -> str:
-    form = LoginForm()
-    if form.validate_on_submit():
-        email = form.email.data
-        if db_funcs.validate_password(email, form.password.data):
-            login_user(db_funcs.get_user_by_email(email), remember=form.remember.data)
-            flash('You have been logged in.', 'success')
-            return redirect(url_for('home'))
-        flash('Login failed. Please try again.', 'danger')
-    return render_template('login.j2', form=form)
-
-
-@app.route('/logout/')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('home'))
-
-
+# Product
 @app.route('/product/add/', methods=['GET', 'POST'])
 @login_required
-def add_product() -> str:
+def add_product() -> Union[Response, str]:
     form = ProductForm()
     if form.validate_on_submit():
         product_id = db_funcs.add_product(name=form.name.data,
@@ -59,8 +30,11 @@ def add_product() -> str:
 
 
 @app.route('/product/<int:product_id>/', methods=['GET', 'POST'])
-def view_product(product_id: int) -> str:
-    product = db_funcs.get_product(product_id)
+def view_product(product_id: int) -> Union[Response, str]:
+    try:
+        product = db_funcs.get_product(product_id)
+    except AttributeError:
+        return redirect(url_for('home'))
     form = RecipeForm(product_id=product_id)
     if form.validate_on_submit():
         db_funcs.add_recipe(product_id=product_id,
@@ -79,100 +53,136 @@ def view_product(product_id: int) -> str:
 @app.route('/products/')
 @login_required
 def products() -> str:
-    products = db_funcs.get_all_products()
+    products = db_funcs.get_all_products(current_user.user_id)
     username = current_user.username
     return render_template('products.j2', username=username, products=products)
 
 
+@app.route('/products/<int:product_id>/share/')
+@login_required
+def share(product_id: int) -> Response:
+    db_funcs.share_product(product_id)
+    return redirect(url_for('view_product', product_id=product_id))
+
+
+# Recipe
 @app.route('/products/<int:product_id>/delete/<int:ingredient_id>/')
 @login_required
-def delete_recipe(product_id: int, ingredient_id: int) -> str:
+def delete_recipe(product_id: int, ingredient_id: int) -> Response:
     db_funcs.delete_recipe(product_id, ingredient_id)
     return redirect(url_for('view_product', product_id=product_id))
 
 
 @app.route('/products/<int:product_id>/delete/all/')
 @login_required
-def delete_product(product_id: int) -> str:
+def delete_product(product_id: int) -> Response:
     db_funcs.delete_product(product_id)
     return redirect(url_for('products'))
 
 
-@app.route('/supplies/')
+# User
+@app.route('/register/', methods=['GET', 'POST'])
+def register() -> Union[Response, str]:
+    form = RegisterForm()
+    if form.validate_on_submit():
+        db_funcs.add_user(username=form.username.data, password=form.password.data,
+                          email=form.email.data, fullname=form.fullname.data,
+                          birthday=form.birthday.data)
+        flash(f'Account created for {form.username.data}.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.j2', form=form)
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login() -> Union[Response, str]:
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        if db_funcs.validate_password(email, form.password.data):
+            login_user(db_funcs.get_user_by_email(email), remember=form.remember.data)
+            flash('You have been logged in.', 'success')
+            return redirect(url_for('home'))
+        flash('Login failed. Please try again.', 'danger')
+    return render_template('login.j2', form=form)
+
+
+@app.route('/logout/')
 @login_required
-def supplies() -> str:
+def logout() -> Response:
+    logout_user()
     return redirect(url_for('home'))
 
 
 @app.route('/profile/<int:user_id>')
-def profile(user_id: int) -> str:
+def profile(user_id: int) -> Union[Response, str]:
     user = db_funcs.get_user(user_id)
+    products = db_funcs.get_all_products(user_id, public_only=True)
     if user is None:
         return redirect(url_for('home'))
-    return render_template('profile.j2', user=user)
+    return render_template('profile.j2', user=user, products=products)
 
 
 @app.route('/profile/username/', methods=['GET', 'POST'])
 @login_required
-def edit_username() -> str:
+def edit_username() -> Union[Response, str]:
     form = UsernameForm()
     if form.validate_on_submit():
         if db_funcs.validate_password(current_user.email, form.password.data):
             db_funcs.change_username(current_user.user_id, form.username.data)
             flash(f'Username changed to {form.username.data}.', 'success')
-            return redirect(url_for('profile'))
+            return redirect(url_for('profile', user_id=current_user.user_id))
         flash('Wrong password. Please try again.', 'danger')
     return render_template('edit_username.j2', form=form)
 
 
 @app.route('/profile/email/', methods=['GET', 'POST'])
 @login_required
-def edit_email() -> str:
+def edit_email() -> Union[Response, str]:
     form = EmailForm()
     if form.validate_on_submit():
         if db_funcs.validate_password(current_user.email, form.password.data):
             db_funcs.change_email(current_user.user_id, form.email.data)
             flash(f'Email changed to {form.email.data}.', 'success')
-            return redirect(url_for('profile'))
+            return redirect(url_for('profile', user_id=current_user.user_id))
         flash('Wrong password. Please try again.', 'danger')
     return render_template('edit_email.j2', form=form)
 
 
 @app.route('/profile/password/', methods=['GET', 'POST'])
 @login_required
-def edit_password() -> str:
+def edit_password() -> Union[Response, str]:
     form = PasswordForm()
     if form.validate_on_submit():
         if db_funcs.validate_password(current_user.email, form.cur_password.data):
             db_funcs.change_password(current_user.user_id, form.new_password.data)
             flash(f'Password changed.', 'success')
-            return redirect(url_for('profile'))
+            return redirect(url_for('profile', user_id=current_user.user_id))
         flash('Wrong password. Please try again.', 'danger')
     return render_template('edit_password.j2', form=form)
 
 
 @app.route('/profile/name/', methods=['GET', 'POST'])
 @login_required
-def edit_name() -> str:
+def edit_name() -> Union[Response, str]:
     form = NameForm()
     if form.validate_on_submit():
         if db_funcs.validate_password(current_user.email, form.password.data):
             db_funcs.change_fullname(current_user.user_id, form.fullname.data)
             flash(f'Name changed to {form.fullname.data}.', 'success')
-            return redirect(url_for('profile'))
+            return redirect(url_for('profile', user_id=current_user.user_id))
         flash('Wrong password. Please try again.', 'danger')
     return render_template('edit_name.j2', form=form)
 
 
 @app.route('/profile/birthday/', methods=['GET', 'POST'])
 @login_required
-def edit_birthday() -> str:
+def edit_birthday() -> Union[Response, str]:
     form = BirthdayForm()
     if form.validate_on_submit():
         if db_funcs.validate_password(current_user.email, form.password.data):
             db_funcs.change_birthday(current_user.user_id, form.birthday.data)
             birthday = form.birthday.data.strftime('%d/%m/%Y')
             flash(f'Date of birth changed to {birthday}.', 'success')
-            return redirect(url_for('profile'))
+            return redirect(url_for('profile', user_id=current_user.user_id))
         flash('Wrong password. Please try again.', 'danger')
     return render_template('edit_birthday.j2', form=form)
